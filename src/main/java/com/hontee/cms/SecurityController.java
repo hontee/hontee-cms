2ea3526de4e445d0f97d168e9bddceb270891850
larry.qi <qzhservice@gmail.com>
2016-02-19 15:50:48 +0800

    添加shiro登录，但失败了。

--- src/main/java/com/hontee/cms/SecurityController.java
+++ src/main/java/com/hontee/cms/SecurityController.java
@@ -1,7 +1,8 @@
 package com.hontee.cms;
 
-import javax.ws.rs.core.Response;
-
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hontee.cms.easyui.vo.Result;
+import com.hontee.cms.easyui.vo.ResultBuilder;
 
 /**
  * Handles requests for the application security page.
@@ -18,19 +22,36 @@ public class SecurityController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SecurityController.class);
 
+	/**
+	 * 进入登陆页面
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		logger.info("Enter login controller page.");
 		return "login";
 	}
 	
+	/**
+	 * 登录提交
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public @ResponseBody Response login(
+	public @ResponseBody Result login(
 			@RequestParam String username, 
 			@RequestParam String password) {
 		logger.info("Handles requests for user login.");
 		
-		return Response.ok().build();
+		try {
+			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+			SecurityUtils.getSubject().login(token);
+		} catch (AuthenticationException e) {
+			return ResultBuilder.failed(e);
+		}
+		
+		return ResultBuilder.ok();
 	}
 
 }
